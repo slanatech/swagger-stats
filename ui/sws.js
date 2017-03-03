fcbbd1b485730d7571bf4425811dd146632b75e0
sv2 <sv2@slana.tech>
2017-03-03 00:38:09 -0800

    Requests by Method, Widgets

--- ui/sws.js
+++ ui/sws.js
@@ -54,7 +54,7 @@
 		this.subscribeEvents();
 
         console.log("Refresh: "+this.options.refreshInterval);
-        setInterval( this.refreshApiStats, this.options.refreshInterval*5000, this );
+        //setInterval( this.refreshApiStats, this.options.refreshInterval*5000, this );
         this.refreshApiStats(this);
 
 
@@ -142,8 +142,30 @@
                         <p><strong>swagger-stats v.0.10.2</strong></p> \
                         <p>Copyright &copy; 2017 <a href="#">SLANATECH</a></p> \
                     </div> \
-                </footer>'
-    };
+                </footer>',
+        datatable: '<div class="col-lg-12"><div class="swsbox float-e-margins"><div class="swsbox-content">\
+                    <div class="table-responsive">\
+                    <table id="%id%" class="table table-striped table-bordered table-condensed table-hover" >\
+                        <thead><tr>%headers%</tr></thead>\
+                        <tbody></tbody>\
+                    </table>\
+                    </div></div></div></div>',
+        tableheader: '<th style="%style%">%title%</th>',
+        widget:       '<div id="%id%" class="col-md-2">\
+                        <div class="swsbox float-e-margins">\
+                         <div class="swsbox-title">\
+                           <span class="sws-widget-extra label pull-right" style="font-size: 12px;"></span>\
+                           <h5 class="sws-widget-title"></h5>\
+                         </div>\
+                         <span class="swsbox-trend sws-widget-trend pull-right"><i class="fa fa-chevron-circle-up"></i></span>\
+                         <div class="swsbox-content">\
+                          <h1 class="sws-widget-value no-margins"></h1>\
+                          <small class="sws-widget-subtitle"></small>\
+                         </div>\
+                        </div>\
+                       </div>'
+
+};
 
     /*
 *   <li class="active"><a href="#main" data-toggle="tooltip" title="Summary"><i class="fa fa-line-chart"></i></a></li> \
@@ -221,6 +243,11 @@
         }
     };
 
+    // Returns percentage string
+    SWSUI.prototype.getPctString = function(val,tot) {
+        return (((val/tot)*100)).toFixed(2).toString()+'%';
+    };
+
     SWSUI.prototype.showSummary = function(swsui) {
 
         // Clear content
@@ -239,23 +266,43 @@
         var elemRow1 = $('<div id="sws-content-summary-row1" class="row">');
         elemContent.append(elemRow1);
 
-        elemRow1.append(swsui.generateNumberWidget('Requests',swsui.apistats.all.requests,'Total received requests'));
-        elemRow1.append(swsui.generateNumberWidget('Responses',swsui.apistats.all.responses,'Total sent responses'));
-        elemRow1.append(swsui.generateNumberWidget('Handle Time',swsui.apistats.all.total_time,'Total Handle Time(ms)'));
-        elemRow1.append(swsui.generateNumberWidget('Avg Handle Time',swsui.apistats.all.avg_time,'Average Handle Time(ms)'));
-        elemRow1.append(swsui.generateNumberWidget('Max Handle Time',swsui.apistats.all.max_time,'Maximum Handle Time(ms)'));
-        elemRow1.append(swsui.generateNumberWidget('Active',(swsui.apistats.all.requests-swsui.apistats.all.responses),'Currently active requests'));
+        // TODO !!! Create widget definition table, index by short id, like s-wRq),
+        // TODO supply parameters there ( like Title, Subtitle, Color rules ...) - except actual values
+
+        elemRow1.append(swsui.createWidget('sws-content-summary-r1-wRq'));
+        swsui.setWidgetValues('sws-content-summary-r1-wRq','Requests',swsui.apistats.all.requests,'Total received requests');
+        elemRow1.append(swsui.createWidget('sws-content-summary-r1-wRe'));
+        swsui.setWidgetValues('sws-content-summary-r1-wRe','Responses',swsui.apistats.all.responses,'Total sent responses');
+        elemRow1.append(swsui.createWidget('sws-content-summary-r1-wHt'));
+        swsui.setWidgetValues('sws-content-summary-r1-wHt','Handle Time',swsui.apistats.all.total_time,'Total Handle Time(ms)');
+        elemRow1.append(swsui.createWidget('sws-content-summary-r1-wAHt'));
+        swsui.setWidgetValues('sws-content-summary-r1-wAHt','Avg Handle Time',swsui.apistats.all.avg_time.toFixed(2),'Average Handle Time(ms)');
+        elemRow1.append(swsui.createWidget('sws-content-summary-r1-wMHt'));
+        swsui.setWidgetValues('sws-content-summary-r1-wMHt','Max Handle Time',swsui.apistats.all.max_time,'Maximum Handle Time(ms)');
+        elemRow1.append(swsui.createWidget('sws-content-summary-r1-wAct'));
+        swsui.setWidgetValues('sws-content-summary-r1-wAct','Active',(swsui.apistats.all.requests-swsui.apistats.all.responses),'Currently active requests');
 
         var elemRow2 = $('<div id="sws-content-summary-row2" class="row">');
         elemContent.append(elemRow2);
-        // TODO Percentage helper
-        var errval = swsui.apistats.all.client_error+swsui.apistats.all.server_error;
-        var errpct = ((errval/swsui.apistats.all.requests)*100).toString()+'%';
-        elemRow2.append(swsui.generateNumberWidget('Errors',errval,'Total Error Responses',errpct));
-        elemRow2.append(swsui.generateNumberWidget('Success',swsui.apistats.all.success,'Success Responses'));
-        elemRow2.append(swsui.generateNumberWidget('Redirect',swsui.apistats.all.redirect,'Redirect Responses'));
-        elemRow2.append(swsui.generateNumberWidget('Client Error',swsui.apistats.all.client_error,'Client Error Responses'));
-        elemRow2.append(swsui.generateNumberWidget('Server Error',swsui.apistats.all.server_error,'Server Error Responses'));
+
+        var totalerrors = swsui.apistats.all.client_error+swsui.apistats.all.server_error;
+        elemRow2.append(swsui.createWidget('sws-content-summary-r2-wErr'));
+        swsui.setWidgetValues('sws-content-summary-r2-wErr','Errors',totalerrors,'Total Error Responses',
+            swsui.getPctString(totalerrors,swsui.apistats.all.requests), totalerrors > 0 ? 'label-danger':'label-success');
+        elemRow2.append(swsui.createWidget('sws-content-summary-r2-wSs'));
+        swsui.setWidgetValues('sws-content-summary-r2-wSs','Success',swsui.apistats.all.success,'Success Responses',
+            swsui.getPctString(swsui.apistats.all.success,swsui.apistats.all.requests));
+        elemRow2.append(swsui.createWidget('sws-content-summary-r2-wRed'));
+        swsui.setWidgetValues('sws-content-summary-r2-wRed','Redirect',swsui.apistats.all.redirect,'Redirect Responses',
+            swsui.getPctString(swsui.apistats.all.redirect,swsui.apistats.all.requests));
+        elemRow2.append(swsui.createWidget('sws-content-summary-r2-wCe'));
+        swsui.setWidgetValues('sws-content-summary-r2-wCe','Client Error',swsui.apistats.all.client_error,'Client Error Responses',
+            swsui.getPctString(swsui.apistats.all.client_error,swsui.apistats.all.requests),
+            swsui.apistats.all.client_error > 0 ? 'label-danger':'label-success');
+        elemRow2.append(swsui.createWidget('sws-content-summary-r2-wSe'));
+        swsui.setWidgetValues('sws-content-summary-r2-wSe','Server Error',swsui.apistats.all.server_error,'Server Error Responses',
+            swsui.getPctString(swsui.apistats.all.server_error,swsui.apistats.all.requests),
+            swsui.apistats.all.server_error > 0 ? 'label-danger':'label-success');
 
         // Timeline
         var elemRow3 = $('<div id="sws-content-summary-row3" class="row">');
@@ -272,6 +319,10 @@
 
         var elemHdr = $('<div class="page-header"><h1>'+this.title+'</h1></div>');
         elemContent.append(elemHdr);
+
+        var elemRow1 = $('<div id="sws-content-requests-row-1" class="row">');
+        elemContent.append(elemRow1);
+        swsui.generateRequestsByMethodTable(elemRow1);
     };
 
 
@@ -289,75 +340,52 @@
     };
 
 
+
+    // Create widget DOM
     // TODO parameter - specify column width (lg-2, lg-3 ... etc )
-    // TODO parameter - color rule ( RED if > 0), always green, etc.
-    // TODO Percentage helper
-    // TODO Color selector for Extra - red / green
-    SWSUI.prototype.generateNumberWidget = function(title, value, subtitle, extra) {
-
-        extra = typeof extra !== 'undefined' ? extra : null;
-        subtitle = typeof subtitle !== 'undefined' ? subtitle : null;
-
-        var widgetHTML = '<div class="col-md-2">';
-        widgetHTML += '<div class="swsbox float-e-margins">';
-        widgetHTML += '<div class="swsbox-title">';
-        if(extra!=null) {
-            widgetHTML += '<span class="stat-percent label label-success pull-right" style="font-size: 12px;">'+extra+'</span>'; // <i class="fa fa-bolt"></i>
-        }
-        widgetHTML += '<h5>'+title+'</h5>';
-        widgetHTML += '</div>';
-        widgetHTML += '<div class="swsbox-content">';
-        widgetHTML += '<h1 class="no-margins" style="color: green;">'+value+'</h1>';
-        if(subtitle!=null) {
-            widgetHTML += '<small>' + subtitle + '</small>';
-        }
-        widgetHTML += '</div>';
-        widgetHTML += '</div>';
-        widgetHTML += '</div>';
-        var elemWidget = $(widgetHTML);
-        return elemWidget;
+    SWSUI.prototype.createWidget = function(wid) {
+        return $( this.template.widget.replace('%id%',wid) );
     };
 
+    // TODO parameter - color rules ( RED if > 0), always green, etc.
+    // TODO Trend (up|down|none)
+    SWSUI.prototype.setWidgetValues = function(wid,title,value,subtitle,extra,extraclass){
+        extra = typeof extra !== 'undefined' ? extra : null;
+        extraclass = (typeof extraclass !== 'undefined' && extraclass!='') ? extraclass : 'label-success';
+        subtitle = typeof subtitle !== 'undefined' ? subtitle : null;
+        var we = $('#'+wid);
+        if(we){
+            we.find('.sws-widget-title').html(title);
+            we.find('.sws-widget-value').html(value);
+            if(subtitle) we.find('.sws-widget-subtitle').html(subtitle);
+            if(extra) we.find('.sws-widget-extra').html(extra).addClass(extraclass);
+            // TODO Columns size
+            // TODO Colors
+        }
+    };
 
     SWSUI.prototype.generateErrorsTable = function(parentElem) {
-        var tableHTML = '<div class="col-lg-12">';
-        tableHTML += '<div class="swsbox float-e-margins">';
-        tableHTML += '<div class="swsbox-content">';
-        tableHTML += '<div class="table-responsive">';
-        tableHTML += '<table class="table table-striped table-bordered table-condensed table-hover sws-table-errors" >';
-        tableHTML += '<thead>';
-        tableHTML += '<tr>';
-        tableHTML += '<th style="width:20%">Time</th>';
-        tableHTML += '<th>Method</th>';
-        tableHTML += '<th style="width:30%">URL</th>';
-        tableHTML += '<th>Code</th>';
-        tableHTML += '<th>Class</th>';
-        tableHTML += '<th>Duration</th>';
-        tableHTML += '<th style="width: 30%">Message</th>';
-        tableHTML += '</tr>';
-        tableHTML += '</thead>';
-        tableHTML += '<tbody>';
-        tableHTML += '</tbody>';
-        tableHTML += '</table>';
-        tableHTML += '</div></div></div></div>';
+
+        var tableHTML = this.template.datatable
+            .replace('%id%','sws-table-errors')
+            .replace('%headers%',this.generateDatatableHeaders(
+                [   ['Time','width:20%;'],['Method',''],['URL','width:30%;'],
+                    ['Code',''],['Class',''],['Duration',''],['Message','width:30%;']
+                ]
+            ));
+
         var elemTable = $(tableHTML);
         parentElem.append(elemTable);
-        var errorsTable = $('.sws-table-errors').DataTable({
+        var errorsTable = $('#sws-table-errors').DataTable({
             pageLength: 25,
             responsive: true,
             dom: '<"html5buttons"B>lTfgitp',
-            buttons: [
-                {extend: 'copy'},
-                {extend: 'csv'},
-                {extend: 'excel', title: 'ExampleFile'},
-                {extend: 'pdf', title: 'ExampleFile'}
-            ],
+            buttons: [ {extend: 'copy'}, {extend: 'csv'} ],
             "order": [[ 0, "desc" ]],
             "createdRow": function ( row, data, index ) {
                $('td', row).eq(1).empty().append('<span class="badge badge-info">'+data[1]+'</span>');
                $('td', row).eq(3).empty().append('<strong>'+data[3]+'</strong>');
             }
-
         });
 
         // Show data
@@ -377,6 +405,67 @@
         }
     };
 
+    //headersDefs: array of pairs [ [<Title>,<style>], ... ]
+    SWSUI.prototype.generateDatatableHeaders = function(headersDefs) {
+        var that = this;
+        var headers = '';
+        headersDefs.forEach(function(v,i,a){
+            headers += that.template.tableheader.replace('%title%',v[0]).replace('%style%',v[1]);
+        });
+        return headers;
+    };
+
+    SWSUI.prototype.generateRequestsByMethodTable = function(parentElem) {
+
+        var tableHTML = this.template.datatable
+            .replace('%id%','sws-table-requestsbymethod')
+            .replace('%headers%',this.generateDatatableHeaders(
+                [ ['Method','width:10%'],
+                  ['Requests',''],['Responses',''],['Errors',''],
+                  ['Success',''],['Redirect',''],['Client Error',''],['Server Error',''],
+                  ['Max Time(ms)',''], ['Avg Time(ms)','']
+                ]
+        ));
+
+        var elemTable = $(tableHTML);
+        parentElem.append(elemTable);
+        var reqByMethodTable = $('#sws-table-requestsbymethod').DataTable({
+            pageLength: 25,
+            responsive: true,
+            bPaginate: false,
+            bFilter:false,
+            bInfo: false,
+            "order": [[ 1, "desc" ]],
+            "createdRow": function ( row, data, index ) {
+                $('td', row).eq(0).empty().append('<span class="badge badge-info">'+data[0]+'</span>');
+                $('td', row).eq(1).empty().append('<strong>'+data[1]+'</strong>');
+                if( data[3] > 0) $('td', row).eq(3).empty().append('<span class="badge badge-danger">'+data[3]+'</span>');
+                if( data[6] > 0) $('td', row).eq(6).empty().append('<span class="badge badge-danger">'+data[6]+'</span>');
+                if( data[7] > 0) $('td', row).eq(7).empty().append('<span class="badge badge-danger">'+data[7]+'</span>');
+            }
+        });
+
+        // Show data
+        if(this.apistats && this.apistats.method){
+            for( var method in this.apistats.method){
+                var reqStats = this.apistats.method[method];
+                reqByMethodTable.row.add([
+                    method,
+                    reqStats.requests,
+                    reqStats.responses,
+                    reqStats.client_error+reqStats.server_error,
+                    reqStats.success,
+                    reqStats.redirect,
+                    reqStats.client_error,
+                    reqStats.server_error,
+                    reqStats.max_time,
+                    reqStats.avg_time.toFixed(2)
+                ]).draw( false );
+            }
+        }
+    };
+
+
 
     SWSUI.prototype.generateTimeline = function(parentElem) {
         var chartHTML = '<div class="col-lg-12">';
@@ -384,7 +473,7 @@
         chartHTML += '<div class="swsbox-content">';
         chartHTML += '<h4>Requests and Responces over last 60 minutes</h4>';
         chartHTML += '<div>';
-        chartHTML += '<canvas id="sws-chart-timeline" height="60"></canvas>';
+        chartHTML += '<canvas id="sws-chart-timeline" height="80px"></canvas>';
         chartHTML += '</div>';
         chartHTML += '</div>';
         chartHTML += '</div>';
