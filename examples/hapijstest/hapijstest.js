17370ed7c629dcf9839c3d4861ba78ddc2a427f6
sv2 <sv2@slana.tech>
2019-08-22 22:27:14 -0700

    #75 - Hapijs support, initial

---
+++ examples/hapijstest/hapijstest.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+var swStats = require('../../lib');    // require('swagger-stats');
+
+const init = async () => {
+
+    const server = Hapi.server({
+        port: 3000,
+        host: 'localhost'
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+            return 'Hello World!';
+        }
+    });
+
+    await server.register({
+        plugin: swStats.getHapiPlugin,
+        options: {}
+    });
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+});
+
+init();
