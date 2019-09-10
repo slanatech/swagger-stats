092176b807295645fb873eeeb0ffaa4719cc61c9
sv2 <sv2@slana.tech>
2019-09-09 22:29:14 -0700

    #51 - Pilot

--- examples/hapijstest/hapijstest.js
+++ examples/hapijstest/hapijstest.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const http = require('http');
 const Hapi = require('@hapi/hapi');
 const swStats = require('../../lib');    // require('swagger-stats');
 
@@ -52,6 +53,15 @@ const init = async () => {
         handler: (request, h) => {
             stopApp();
             return 'STOP';
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/request',
+        handler: (request, h) => {
+            testEgressRequest();
+            return 'OK';
         }
     });
 
@@ -194,6 +204,30 @@ async function testerImpl(request,h) {
     }
 
     return h.response({code: code, message: message}).code(code);
+}
+
+function testEgressRequest(){
+    const options = {
+        hostname: 'www.google.com',
+        port: 80,
+        path: '/',
+        method: 'GET',
+    };
+    const req = http.request(options, (res) => {
+        console.log(`STATUS: ${res.statusCode}`);
+        console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+        res.setEncoding('utf8');
+        res.on('data', (chunk) => {
+            console.log(`BODY: ${chunk}`);
+        });
+        res.on('end', () => {
+            console.log('No more data in response.');
+        });
+    });
+    req.on('error', (e) => {
+        console.error(`problem with request: ${e.message}`);
+    });
+    req.end();
 }
 
 
